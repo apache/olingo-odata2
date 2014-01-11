fa108dca88d8df65986e7ee5256da5e37ea44b88
Chandan V A <chandan.v.a@sap.com>
2014-01-11 20:55:27 +0530

    [OLINGO-56] - Enhance the Data Model and the mock data for the reference scenario.
    Signed-off-by: Chandan V A <chandan.v.a@sap.com>

--- odata2-jpa-processor/jpa-web/src/main/java/org/apache/olingo/odata2/jpa/processor/ref/extension/_SalesOrderHeaderProcessor.java
+++ odata2-jpa-processor/jpa-web/src/main/java/org/apache/olingo/odata2/jpa/processor/ref/extension/_SalesOrderHeaderProcessor.java
@@ -108,7 +108,7 @@ public class _SalesOrderHeaderProcessor {
     List<SalesOrderHeader> soList = (List<SalesOrderHeader>) q
         .getResultList();
     if (!soList.isEmpty()) {
-      return soList.get(0).getBuyerAddress();
+      return soList.get(0).getCustomer().getAddress();
     } else {
       return null;
     }
