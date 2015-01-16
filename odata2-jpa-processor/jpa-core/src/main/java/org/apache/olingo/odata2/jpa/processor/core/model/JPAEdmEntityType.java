9d63ef43219eeafc9ab2f2de8270333952258aa5
Chandan V A <chandan.v.a@sap.com>
2015-01-16 09:46:32 +0530

    [OLINGO-537] - Fix for ignoring EntityListeners getting added to JPAEdmMapping if it does not inherit from ODataJPATombstoneEntityListener

--- odata2-jpa-processor/jpa-core/src/main/java/org/apache/olingo/odata2/jpa/processor/core/model/JPAEdmEntityType.java
+++ odata2-jpa-processor/jpa-core/src/main/java/org/apache/olingo/odata2/jpa/processor/core/model/JPAEdmEntityType.java
@@ -113,15 +113,13 @@ public class JPAEdmEntityType extends JPAEdmBaseViewImpl implements JPAEdmEntity
         JPAEdmMapping jpaEdmMapping = (JPAEdmMapping) currentEdmEntityType.getMapping();
         EntityListeners entityListners = currentJPAEntityType.getJavaType().getAnnotation(EntityListeners.class);
         if (entityListners != null) {
-          for (Class<? extends ODataJPATombstoneEntityListener> entityListner : entityListners.value())
+          for (Class<EntityListeners> entityListner : entityListners.value())
           {
-            try {
-              jpaEdmMapping.setODataJPATombstoneEntityListener(entityListner);
-              break;
-            } catch (ClassCastException e) {
-              continue;
-            }
-
+              if (ODataJPATombstoneEntityListener.class.isAssignableFrom(entityListner)) {
+                jpaEdmMapping
+                    .setODataJPATombstoneEntityListener((Class<? extends ODataJPATombstoneEntityListener>) entityListner);
+                break;
+              }
           }
         }
         JPAEdmPropertyView propertyView = new JPAEdmProperty(schemaView);
