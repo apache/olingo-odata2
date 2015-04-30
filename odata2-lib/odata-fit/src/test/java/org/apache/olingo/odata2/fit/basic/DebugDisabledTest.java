1d8b7b916d30380c26198ebed97f236b896fbbe0
Christian Amend <chrisam@apache.org>
2015-04-30 14:09:12 +0200

    [OLINGO-646] Start fixing debug message

---
+++ odata2-lib/odata-fit/src/test/java/org/apache/olingo/odata2/fit/basic/DebugDisabledTest.java
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ ******************************************************************************/
+package org.apache.olingo.odata2.fit.basic;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.apache.http.HttpResponse;
+import org.apache.olingo.odata2.api.commons.HttpStatusCodes;
+import org.apache.olingo.odata2.api.exception.ODataException;
+import org.apache.olingo.odata2.api.processor.ODataResponse;
+import org.apache.olingo.odata2.api.processor.ODataSingleProcessor;
+import org.apache.olingo.odata2.api.processor.part.ServiceDocumentProcessor;
+import org.apache.olingo.odata2.api.uri.info.GetServiceDocumentUriInfo;
+import org.apache.olingo.odata2.testutil.fit.DebugCallbackFactoryFlase;
+import org.apache.olingo.odata2.testutil.fit.DebugCallbackFactoryTrue;
+import org.apache.olingo.odata2.testutil.fit.FitStaticServiceFactory;
+import org.apache.olingo.odata2.testutil.helper.StringHelper;
+import org.apache.olingo.odata2.testutil.server.ServletType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class DebugDisabledTest extends AbstractBasicTest {
+
+  public DebugDisabledTest(final ServletType servletType) {
+    super(servletType);
+  }
+
+  @Test
+  public void checkNoDebugCallbackMustResultInNoDebugResponse() throws Exception {
+    startCustomServer(FitStaticServiceFactory.class);
+    HttpResponse response = executeGetRequest("/?odata-debug=json");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    String payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    response = executeGetRequest("/?odata-debug=html");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    response = executeGetRequest("/?odata-debug=download");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    stopCustomServer();
+  }
+
+  @Test
+  public void checkDebugCallbackFalseMustResultInNoDebugResponse() throws Exception {
+    startCustomServer(DebugCallbackFactoryFlase.class);
+    HttpResponse response = executeGetRequest("/?odata-debug=json");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    String payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    response = executeGetRequest("/?odata-debug=html");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    response = executeGetRequest("/?odata-debug=download");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertEquals("service document", payload);
+
+    stopCustomServer();
+  }
+
+  @Test
+  public void checkDebugCallbackTrueMustResultInDebugResponse() throws Exception {
+    startCustomServer(DebugCallbackFactoryTrue.class);
+    HttpResponse response = null;
+    String payload = null;
+    response = executeGetRequest("/?odata-debug=json");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertFalse("service document".equals(payload));
+    assertTrue(payload.startsWith("{\"request\":{\"method\":\"GET\""));
+    assertTrue(payload.contains("service document"));
+
+    response = executeGetRequest("/?odata-debug=html");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertFalse("service document".equals(payload));
+    assertTrue(payload.startsWith("<!DOCTYPE html"));
+    assertTrue(payload.contains("service document"));
+
+    response = executeGetRequest("/?odata-debug=download");
+    assertEquals(HttpStatusCodes.OK.getStatusCode(), response.getStatusLine().getStatusCode());
+    payload = StringHelper.inputStreamToString(response.getEntity().getContent());
+    assertFalse("service document".equals(payload));
+    assertTrue(payload.contains("service document"));
+
+    stopCustomServer();
+  }
+
+  @Override
+  @Before
+  public void before() {
+    // Do nothing here to stop default server from starting
+  }
+
+  @Override
+  @After
+  public void after() {
+    // Do nothing here to stop default server from ending
+  }
+
+  @Override
+  protected ODataSingleProcessor createProcessor() throws ODataException {
+    final ODataSingleProcessor processor = mock(ODataSingleProcessor.class);
+    when(
+        ((ServiceDocumentProcessor) processor).readServiceDocument(any(GetServiceDocumentUriInfo.class),
+            any(String.class)))
+        .thenReturn(ODataResponse.entity("service document").status(HttpStatusCodes.OK).build());
+    return processor;
+  }
+
+}
