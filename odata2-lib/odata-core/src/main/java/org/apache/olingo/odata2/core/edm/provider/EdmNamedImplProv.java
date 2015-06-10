96c28146e9b02c7fbd670813e7b6c43e15fe5050
Michael Bolz <michael.bolz@sap.com>
2015-06-10 09:48:16 +0200

    [OLINGO-689] Fixed name validation pattern for EdmNamed

--- odata2-lib/odata-core/src/main/java/org/apache/olingo/odata2/core/edm/provider/EdmNamedImplProv.java
+++ odata2-lib/odata-core/src/main/java/org/apache/olingo/odata2/core/edm/provider/EdmNamedImplProv.java
@@ -26,14 +26,12 @@ import org.apache.olingo.odata2.api.edm.EdmNamed;
 
 public abstract class EdmNamedImplProv implements EdmNamed {
 
-  private static final Pattern PATTERN_VALID_NAME = Pattern
-      .compile("^[:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d"
-          + "\\u037f-\\u1fff\\u200c\\u200d\\u2070-\\u218f\\u2c00-\\u2fef\\u3001-\\ud7ff"
-          + "\\uf900-\\ufdcf\\ufdf0-\\ufffd\\x10000-\\xEFFFF]"
-          + "[:A-Z_a-z\\u00C0\\u00D6\\u00D8-\\u00F6"
-          + "\\u00F8-\\u02ff\\u0370-\\u037d\\u037f-\\u1fff\\u200c\\u200d\\u2070-\\u218f"
-          + "\\u2c00-\\u2fef\\u3001-\\udfff\\uf900-\\ufdcf\\ufdf0-\\ufffd\\-\\.0-9"
-          + "\\u00b7\\u0300-\\u036f\\u203f-\\u2040]*\\Z");
+  private static final Pattern PATTERN_VALID_NAME = Pattern.compile(
+      "\\A[:A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d\\u200c\\u200d\\u2070-\\u218f"
+      + "\\u037f-\\u1fff\\u2c00-\\u2fef\\u3001-\\ud7ff\\uf900-\\ufdcf\\ufdf0-\\ufffd]"
+      + "[:A-Z_a-z\\u00C0-\\u00D6\\u00D8-\\u00F6\\u00F8-\\u02ff\\u0370-\\u037d\\u200c\\u200d\\u2070-\\u218f"
+      + "\\u037f-\\u1fff\\u2c00-\\u2fef\\u3001-\\udfff\\uf900-\\ufdcf\\ufdf0-\\ufffd\\-\\.0-9\\u00b7\\u0300-\\u036f]*"
+      + "\\Z");
   protected EdmImplProv edm;
   private String name;
 
