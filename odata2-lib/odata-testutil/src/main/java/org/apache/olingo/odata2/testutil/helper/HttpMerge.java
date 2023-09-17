a753ad666831ebbd3c6055aa5bdaef1c199c451d
mibo <mibo@apache.org>
2023-09-17 08:45:24 +0200

    [OLINGO-1604] Removed deprecated annotations

--- odata2-lib/odata-testutil/src/main/java/org/apache/olingo/odata2/testutil/helper/HttpMerge.java
+++ odata2-lib/odata-testutil/src/main/java/org/apache/olingo/odata2/testutil/helper/HttpMerge.java
@@ -20,10 +20,8 @@ package org.apache.olingo.odata2.testutil.helper;
 
 import java.net.URI;
 
-import org.apache.http.annotation.NotThreadSafe;
 import org.apache.http.client.methods.HttpRequestBase;
 
-@NotThreadSafe
 public class HttpMerge extends HttpRequestBase {
 
   public final static String METHOD_NAME = "MERGE";
